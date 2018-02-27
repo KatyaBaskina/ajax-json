588bc24257e563a54a8ac5184a06b1a6be01c4b1
KatyaBaskina <34843845+KatyaBaskina@users.noreply.github.com>
2018-02-27 23:53:22 +0200

    new

---
+++ index.php
@@ -0,0 +1,84 @@
+<?php
+require __DIR__ . "/back_end/model.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="wrapper">
+        <div id="container">
+            <div class="items">   
+                <?php foreach (getItems(1, 4) as $item): ?>
+                    <div class="item">
+                        <div class="img">
+                            <img src="<?php echo $item['img']; ?>" alt="<?php echo $item['title']; ?>">
+                        </div>
+                        <h1><?php echo $item['title']; ?></h1>
+                        <p><?php echo $item['description']; ?></p>
+                        
+                        <span class="cost">
+                            <?php echo $item['discountCost'] ? $item['discountCost'] : $item['cost']; ?>
+                        </span>
+
+                        <?php if ($item['discountCost'] !== null): ?>
+                            <span class="red_cost">
+                                <?php echo $item['cost']; ?>
+                            </span>
+                            <div class="sale"><span>Sale</span></div>
+                        <?php endif; ?>
+                        <?php if ($item['new']): ?>
+                            <div class="new"><span>New</span></div>
+                        <?php endif; ?>
+                        <div class="btns">
+                            <a href="#" class="btn add"><span>add to cart</span></a>
+                            <a href="#" class="btn view"><span>view</span></a>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+        <button id="btn" class="load"><span>Load more</span></button>
+        <div class="cards">
+            <div class="item">
+                <h1>hot offers</h1>
+                <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae. Suspendisse sollicitudin velit sed leo. Ut pharetra augue nec augue. Nam elit magna</p>
+                <ul>
+                    <li><span>Vestibulum ante ipsum primis in faucibus orci luctus</span></li>
+                    <li><span>Nam elit magna hendrerit sit amet tincidunt ac</span></li>
+                    <li><span>Quisque diam lorem interdum vitae dapibus ac scele</span></li>
+                    <li><span>Donec eget tellus non erat lacinia fermentum</span></li>
+                    <li><span>Donec in velit vel ipsum auctor pulvin</span></li>
+                </ul>
+            </div>
+            <div class="item">
+                <h1>hot offers</h1>
+                <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae. Suspendisse sollicitudin velit sed leo. Ut pharetra augue nec augue. Nam elit magna</p>
+                <ul>
+                    <li><span>Vestibulum ante ipsum primis in faucibus orci luctus</span></li>
+                    <li><span>Nam elit magna hendrerit sit amet tincidunt ac</span></li>
+                    <li><span>Quisque diam lorem interdum vitae dapibus ac scele</span></li>
+                    <li><span>Donec eget tellus non erat lacinia fermentum</span></li>
+                    <li><span>Donec in velit vel ipsum auctor pulvin</span></li>
+                </ul>
+            </div>
+            <div class="item info">
+                <h1>Store information</h1>
+                <ul>
+                    <li><span>Company Inc., 8901 Marmora Road, Glasgow, D04 89GR</span></li>
+                    <li><span>Call us now toll free: (800) 2345-6789</span></li>
+                    <li><span>Customer support: support@example.com <br> Press: pressroom@example.</span></li>
+                    <li><span>Skype: sample-username</span></li>
+                </ul>
+            </ul>
+        </div>
+    </div>
+    <script src="js/script.js"></script>
+</div>
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,84 @@
+var goodsContainer;
+var get_page = 2;
+var get_perpage = 4;
+var htmlString = '';
+document.addEventListener('DOMContentLoaded', function(){
+
+	goodsContainer = document.getElementById('container');
+	var params = getParams(get_page, get_perpage);
+	ajaxGet(params);
+});
+
+document.getElementById('btn').onclick = function(){
+	addElement(htmlString);
+	htmlString = '';
+	var params = getParams(get_page, get_perpage);
+	ajaxGet(params);
+}
+
+var ajaxGet = function(params){
+	var request = new XMLHttpRequest();
+
+	request.onreadystatechange = function(){
+		if(request.readyState == 4 && request.status == 200){
+			var getData = JSON.parse(request.responseText);
+			renderHTML(getData);
+			get_page++;
+			if(getData['entities'][0] == undefined){
+				document.getElementById('btn').style.display = 'none';
+			}
+		}
+	}
+
+	request.open('GET', 'back_end/list.php'+'?'+params);
+	request.send();
+}
+
+function renderHTML(data){
+
+	for (i = 0; i < data['entities'].length; i++){
+		var discountCost = (data['entities'][i].discountCost) ? data['entities'][i].discountCost : data['entities'][i].cost;
+		var sale = '';
+		var new_ = '';
+		var redCost = '';
+		if(data['entities'][i].discountCost){
+			redCost = '<span class="red_cost">' + data['entities'][i].cost + '</span>';
+			sale = '<div class="sale"><span>Sale</span></div>'
+		}
+		if(data['entities'][i].new)
+			new_= '<div class="new"><span>new</span></div>'
+		htmlString += '<div class="item">' +
+					  		'<div class="img">' +
+		 			  			'<img src=	\'' + data['entities'][i].img + '\' ' + 'alt=\'' + data['entities'][i].title + '\'>' + 
+		 			  		'</div>' +
+		 			  		'<h1>' + data['entities'][i].title + '</h1>' +
+		 			  		'<p>' + data['entities'][i].description + '</p>' +
+		 			  		'<span class="cost">' + discountCost + '</span>' +
+		 			  		redCost +
+		 			  		sale + new_ +
+		 			  		'<div class="btns">'+
+		 			  			'<a href="#" class="btn add">' + '<span>add to cart</span>' + '</a>' +
+		 			  			'<a href="#" class="btn view">' + '<span>view</span>' + '</a>' +
+		 			  		'</div>' +
+		 			  '</div>';
+	}
+}
+
+function getParams(page, perPage){
+	var params = 'page=' + get_page + '&' + 'per_page=' + get_perpage;
+	return params;
+}
+
+addElement = function(htmlString){
+	var newElement = document.createElement('div');
+	newElement.classList.add('items');
+	newElement.innerHTML= htmlString;
+	newElement.style.opacity = 0;
+	newElement.style.transition = 'opacity 2s';
+
+	goodsContainer.appendChild(newElement);
+	window.setTimeout(function(){
+		newElement.style.opacity = 1;
+	}, 100);
+
+}
